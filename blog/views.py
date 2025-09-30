b9421e75b1061d4b5483e5a555e6799edee24884
EmmanuelIdeho <ohis98@gmail.com>
2025-09-30 18:49:51 -0400

    create Comment model

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Post
+from .models import Post, Comment
 from .forms import CommentForm, PostForm
 from django.utils import timezone
 
@@ -59,6 +59,18 @@ def post_remove(request, pk):
     if request.method == 'POST':
         post.delete()
     return redirect('post_list')
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
